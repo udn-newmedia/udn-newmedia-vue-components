0a7d46fae64e913704e53648d9b4c01bfb273618
Billy.Liao <billfulo@gmail.com>
2017-11-13 13:59:44 +0800

    add HeadBar

--- components/Header.vue
+++ components/Header.vue
@@ -3,14 +3,16 @@
         <div id="icon">
             <a href="./index"><img src="https://udn.com/upf/newmedia/image/udn.png" title="聯合報"></a>
         </div>
-        <div id="hbutton-contain" :class="{transformToNone: isOpen}">
+        <div id="hbutton-contain" :class="{transformToNone: isOpen}" :style="{transform: menuSlideDirection}">
             <slot></slot>
             <div id="logo-contain" class="hidden-lg">
                 <Logo />
             </div>
         </div>
         <div id="hbutton" class="hidden-lg">
-            <div id="nav-icon" @click="handleBurger()" :class="{open: isOpen}">
+            <div class="nav-icon" :class="{open: isOpen}"
+                @click="handleBurger()"
+            >
                 <span></span>
                 <span></span>
                 <span></span>
@@ -26,16 +28,17 @@ import Logo from '../components/Logo.vue';
 export default {
 
     name: 'HeadBar',
-    props: ['BgColor'],
+    props: ['BgColor', 'MenuSlideFrom'],
     components: { Logo },
     data: function() {
         return {
             top: 0,
             isOpen: false,
+            menuSlideDirection: "",
         }
     },
-    mounted: function() {
-        window.addEventListener('scroll', this.handleScroll);
+    computed: {
+
     },
     methods: {
         handleScroll: function(event) {
@@ -53,7 +56,35 @@ export default {
         handleBurger: function(event) {
             this.isOpen == false ? this.isOpen = true : this.isOpen = false;
         }
-    }
+    },
+    created: function() {
+        console.log(this)
+        switch(this.MenuSlideFrom) {
+            case 'top':
+                this.menuSlideDirection = 'translate(0, -110%)';
+                break;
+
+            case 'bottom':
+                this.menuSlideDirection = 'translate(0, 100%)';
+                break;
+
+            case 'left':
+                this.menuSlideDirection = 'translate(-100%, 0)';
+                break;
+
+            case 'right':
+                this.menuSlideDirection = 'translate(100%, 0)';
+                break;
+
+            default:    
+                this.menuSlideDirection = 'translate(0, -110%)';
+                console.log("please set value is top, bottom, left or right") 
+                break;
+        }
+    },
+    mounted: function() {
+        window.addEventListener('scroll', this.handleScroll);
+    },    
 }
 
 </script>
@@ -70,13 +101,14 @@ export default {
 
 #icon {
     position: absolute;
-    z-index: 0;
+    z-index: 30;
     top: 0;
 }
 
 #icon img {
     width: 130px;
     margin-top: -3px;
+    z-index: 30;
 }
 
 #hbutton {
@@ -86,71 +118,68 @@ export default {
     right: 0;
     padding: 0 5px;
     position: absolute;
+    z-index: 30;
 }
 
-#nav-icon {
+.nav-icon {
     width: 46px;
     height: 46px;
+    z-index: 30;
     position: relative;
     transform: rotate(0deg);
     transition: .5s ease-in-out;
     cursor: pointer;
     opacity: 1;
+    span {
+        display: block;
+        position: absolute;
+        height: 4px;
+        width: 30px;
+        margin: 0 auto;
+        background: #FFFFFF;
+        border-radius: 2px;
+        opacity: 1;
+        right: 12px;
+        transform: rotate(0deg);
+        transition: .66s ease-in-out;        
+    }
+    span:nth-child(1){
+        top: 12px;
+        transition: .44s ease-out;
+        transform-origin: right;    
+    }
+    span:nth-child(2),
+    span:nth-child(3){
+        top: 21px;    
+    }
+    span:nth-child(4){
+        top: 30px;    
+        transition: .44s ease-out;
+    }
 }
-
-#nav-icon span {
-    display: block;
-    position: absolute;
-    height: 4px;
-    width: 30px;
-    margin: 0 auto;
-    background: #FFFFFF;
-    border-radius: 2px;
-    opacity: 1;
-    right: 12px;
-    transform: rotate(0deg);
-    transition: .25s ease-in-out;
+.open{
+    span:nth-child(1){
+        width: 0;
+    }
+    span:nth-child(2){
+        transform: rotate(45deg);
+    }
+    span:nth-child(3){
+        transform: rotate(-45deg);
+    }
+    span:nth-child(4){
+        width: 0;     
+    }        
 }
-
-#nav-icon span:nth-child(1) {
-    top: 12px;
-}
-
-#nav-icon span:nth-child(2),
-#nav-icon span:nth-child(3) {
-    top: 21px;
-}
-
-#nav-icon span:nth-child(4) {
-    top: 30px;
-}
-
-#nav-icon.open span:nth-child(1) {
-    top: 12px;
-    width: 0%;
-}
-
-#nav-icon.open span:nth-child(2) {
-    transform: rotate(45deg);
-}
-
-#nav-icon.open span:nth-child(3) {
-    transform: rotate(-45deg);
-}
-
-#nav-icon.open span:nth-child(4) {
-    top: 30px;
-    width: 0%;
-}
-
 #hbutton-contain {
     position: absolute;
+    z-index: 0;
     cursor: pointer;
     top: 0;
     right: 0;
     background-clip: content-box;
+    transform: translate(0, -110%);
 }
-
 .hbutton {
     padding: 0 12px;
     color: #FFFFFF;
@@ -189,7 +218,6 @@ export default {
         height: 100vh;
         width: 100%;
         transition: transform 0.7s ease;
-        transform: translate(110%, 0);
     }
     #hbutton-contain.open {
         transform: translate(0, 0);
@@ -222,9 +250,6 @@ export default {
     #icon {
         height: 50px;
         line-height: 50px;
-    }
-    .hidden-lg{
-        display: none;
     }
 }
 
