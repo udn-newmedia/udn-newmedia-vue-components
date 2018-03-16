ea5ad377e0366b0b15fb024a5d263ad2a6e74fbd
udn-newmedia@gmail.com <waiting0819@msn.com>
2018-03-16 14:19:18 +0800

    chnage policy link

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -4,7 +4,7 @@
             <span class="footer-link footer-right-board"><a href="http://www.udngroup.com/" target="_blank">聯合報系</a></span>
             <span class="footer-link footer-right-board"><a href="https://www.facebook.com/udnplus" target="_blank">粉絲團</a></span>
             <span class="footer-link footer-right-board"><a href="http://udndata.com/udnauthority.html" target="_blank">新聞授權</a></span>
-            <span class="footer-link"><a href="http://www.udngroup.com/information/privacy" target="_blank">隱私權聲明</a></span>
+            <span class="footer-link"><a href="http://www.udngroup.com/members/udn_privacy" target="_blank">隱私權聲明</a></span>
         </div>
     </div>
 </template>
