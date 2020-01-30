a61149fb1b71f9a52510ee082ca6509999d5500d
Freddy Yang <freddy10100@hotmail.com>
2020-01-30 11:59:18 +0800

    babel preset-env & file-loader

--- src/components/Footer/CopyRight.vue
+++ src/components/Footer/CopyRight.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="copy-right">
-
+    a
+    <img :src="cortana" alt="">
   </div>
 </template>
 
 <script>
+import cortana from '~/cortana.jpg';
+
 export default {
   name: 'CopyRight',
+  data() {
+    return {
+      cortana: cortana,
+    }
+  },
 }
 </script>
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,66 +2,79 @@
 const webpack = require("webpack");
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
   entry: "./src/main.js",
   output: {
     filename: "udn-newmedia-vue-components.min.js",
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "./"
   },
-  mode: 'development',
+  mode: "development",
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.scss$/,
         use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
+          "vue-style-loader",
+          "css-loader",
+          "sass-loader"
         ]
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
         exclude: file => (
           /node_modules/.test(file) &&
           !/\.vue\.js/.test(file)
-        )
+          ),
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+          outputPath: 'assets'
+        }
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
-      },
+        loader: "vue-loader"
+      }
     ]
   },
   resolve: {
-    extensions:['.vue','.js'],
-    alias:{
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': path.resolve(__dirname, 'src')
+    extensions: [".vue", ".js"],
+    alias: {
+      "vue$": "vue/dist/vue.esm.js",
+      "@": path.resolve(__dirname, "src"),
+      "~": path.resolve(__dirname, "assets")
     }
   },
   plugins: [
     new VueLoaderPlugin(),
     new CleanWebpackPlugin(),
-    // new HtmlWebpackPlugin({
-    //   template: './index.html'
-    // }),
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    }),
   ],
   optimization: {
     minimizer: [new UglifyJsPlugin()],
   },
   devServer: {
-    index: './index.html',
+    index: "./index.html",
+    publicPath: "/",
     hot: true,
     port: 8080,
     open: true,
     // noInfo: true,
-    overlay: {
-      errors: true
-    }
+    overlay: true
   },
 };
